804e702c368c080ab28ec47fc3c426ef3a0ac2f7
PollyOliv <pollyoliveira_@outlook.com>
2023-11-18 10:34:57 -0300

    Alteração de material

--- JS/variaveis.js
+++ JS/variaveis.js
@@ -29,3 +29,33 @@ function exemploConst(){
 }
 
 console.log(x);
+
+function tiposVariaveis(){
+    let nome = "Francisco"
+    let idade = 18
+    let altura = 1.74
+
+
+    let verdade = true
+    let falso = false
+    let lista = ['carro','computador','casa']
+    
+    
+    let vazio = null
+
+    let sem_valor;
+
+    let aluno = {
+
+        'nome':nome,
+        'idade': idade,
+        'altura':altura,
+    }
+
+    console.log(nome, idade, altura)
+    console.log(verdade, falso)
+    console.log(lista)
+    console.log(vazio)
+    console.log(sem_valor)
+    console.log(aluno)
+}
